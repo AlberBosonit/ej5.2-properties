34911abac3a628f3c3a03a8c7c01fb641448c073
Alberto Garcia <alberto.gdepablos@bosonit.com>
2022-09-15 16:03:19 +0200

    Añadido mostrar valor de variables de entorno del S.O.

--- src/main/java/com/example/ej5/properties/Application.java
+++ src/main/java/com/example/ej5/properties/Application.java
@@ -5,12 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
 	@Autowired
 	Properties properties;
+	@Autowired
+	Environment environment;
 
 	public static void main(String[] args){
 		SpringApplication.run(Application.class, args);
@@ -21,7 +24,8 @@ public class Application implements CommandLineRunner {
 		System.out.println("El valor de greeting es: "+properties.getGreeting());
 		System.out.println("El valor de my.number es: "+properties.getMyNumber());
 		System.out.println("El valor de new.property es: "+properties.getNewProperty());
-
+		String javaHome = environment.getProperty("JAVA_HOME");
+		System.out.println(javaHome);
 
 	}
 }
